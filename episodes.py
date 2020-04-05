656b36e530ebd409acd28a267f8a9eadaad8201b
Mahyar Abolhallaj <42915606+mabolhal@users.noreply.github.com>
2020-04-05 18:22:08 -0400

    Update episodes.py

--- episodes.py
+++ episodes.py
@@ -1,13 +1,11 @@
 import praw
 
-client_id = 'Q6LswfYdWjvyzA'
-client_secret = 'z-g7WIqhWsJkhR6e8xwU5c9PuTs'
-user_agent = 'Testing API'
+client_id = '-'
+client_secret = '-'
+user_agent = '-'
 
 reddit = praw.Reddit(client_id=client_id, client_secret=client_secret,user_agent=user_agent)
 
-# so let's see if we can automate this process
-# # First let's see if we can get the submission id from the title
 episodes = []
 rpdr = reddit.subreddit('rupaulsdragrace')
 for n in range(1,13):
